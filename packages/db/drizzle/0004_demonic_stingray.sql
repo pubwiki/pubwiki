4677c68d1358b11632251de662b6046c7e09b9b7
M4tsuri <me@m4tsuri.com>
2026-03-28 19:08:43 +0800

    Fixed db migration

--- packages/db/drizzle/0004_demonic_stingray.sql
+++ packages/db/drizzle/0004_demonic_stingray.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `save_contents` ADD `save_encoding` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `save_contents` ADD `save_encoding` text NOT NULL DEFAULT 'keyframe';--> statement-breakpoint
 ALTER TABLE `save_contents` ADD `parent_commit` text;
